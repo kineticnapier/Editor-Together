46f21694ce08a8834922ce104a6377981d4cc75c
Kinetic Napier <u61819269@gmail.com>
2026-09-25 22:38:39 +0900

    Detect editor mutations through scnEditor.SaveState

--- src/EditorCollaboration/Patches/SaveStateScopePatch.cs
+++ src/EditorCollaboration/Patches/SaveStateScopePatch.cs
@@ -1,62 +1,21 @@
-using System;
-using System.Reflection;
 using HarmonyLib;
 
 namespace EditorCollaboration.Patches
 {
-    [HarmonyPatch]
+    // SaveState is the stable mutation boundary in the stock editor. SaveStateScope
+    // is an implementation detail and its runtime type/constructor shape can move
+    // between ADOFAI builds, while mutating editor operations ultimately save a
+    // full LevelData snapshot through scnEditor.SaveState.
+    [HarmonyPatch(typeof(scnEditor), nameof(scnEditor.SaveState))]
     internal static class SaveStateScopePatch
     {
-        private static Type ScopeType => AccessTools.TypeByName("SaveStateScope");
-
-        [HarmonyTargetMethods]
-        private static System.Collections.Generic.IEnumerable<MethodBase> TargetMethods()
-        {
-            Type type = ScopeType;
-            if (type == null)
-                yield break;
-
-            ConstructorInfo ctor = AccessTools.Constructor(type, new[]
-            {
-                typeof(scnEditor), typeof(bool), typeof(bool), typeof(bool)
-            });
-            if (ctor != null)
-                yield return ctor;
-
-            MethodInfo dispose = AccessTools.Method(type, "Dispose");
-            if (dispose != null)
-                yield return dispose;
-        }
-
-        [HarmonyPrefix]
-        private static void Prefix(MethodBase __originalMethod, object[] __args)
-        {
-            if (__originalMethod is ConstructorInfo)
-            {
-                if (__args.Length < 4)
-                    return;
-
-                Main.Controller?.OnScopeEntering(
-                    (scnEditor)__args[0],
-                    (bool)__args[2],
-                    (bool)__args[3]);
-            }
-        }
-
         [HarmonyPostfix]
-        private static void Postfix(MethodBase __originalMethod, object __instance)
+        private static void Postfix(scnEditor __instance, bool clearRedo = true, bool dataHasChanged = true)
         {
-            if (__originalMethod is ConstructorInfo)
+            if (__instance == null || !dataHasChanged)
                 return;
 
-            scnEditor editor = null;
-            if (__instance != null)
-            {
-                FieldInfo field = AccessTools.Field(__instance.GetType(), "editor");
-                editor = field?.GetValue(__instance) as scnEditor;
-            }
-
-            Main.Controller?.OnScopeDisposed(editor);
+            Main.Controller?.OnEditorStateSaved(__instance);
         }
     }
 }
