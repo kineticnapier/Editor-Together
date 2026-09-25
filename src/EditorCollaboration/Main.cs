5f8e31a7ce1ed2a215dff2d227b566b85a580501
Kinetic Napier <u61819269@gmail.com>
2026-09-25 20:59:12 +0900

    Pump collaboration receive queue on Unity update

--- src/EditorCollaboration/Main.cs
+++ src/EditorCollaboration/Main.cs
@@ -16,9 +16,15 @@ namespace EditorCollaboration
             Harmony = new Harmony(modEntry.Info.Id);
             Harmony.PatchAll(typeof(Main).Assembly);
 
+            modEntry.OnUpdate = OnUpdate;
             modEntry.OnUnload = Unload;
             modEntry.Logger.Log("EditorCollaboration v0.0.1 prototype loaded.");
             return true;
+        }
+
+        private static void OnUpdate(UnityModManager.ModEntry modEntry, float deltaTime)
+        {
+            Controller?.Update();
         }
 
         private static bool Unload(UnityModManager.ModEntry modEntry)
